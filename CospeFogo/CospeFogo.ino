35bdd29c87a8464ba42a8f25f4937100b2901c23
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-09-12 15:32:42 -0300

    Works with a candle

--- CospeFogo/CospeFogo.ino
+++ CospeFogo/CospeFogo.ino
@@ -1,24 +1,53 @@
-const int  buttonPin = 2;
-int buttonState = 0;
-int lastButtonState = 0;
+#include <IRremote.h>
+#include <Servo.h>
+/*
+  const int  buttonPin = 8;
+  int buttonState = 0;
+  int lastButtonState = 0;
+  const int relePin = 6;
+*/
+
+const int servoPin = 9;
+int IR_Recv = 7;
+Servo servo;
+IRrecv irrecv(IR_Recv);
+decode_results result;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
+  servo.attach(servoPin);
+  servo.write(0);
+  irrecv.enableIRIn();
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
+  if (irrecv.decode(&result)) {
+    long int codigo = result.value;
+    Serial.println(result.value);
+    irrecv.resume();
 
-  if (buttonState != lastButtonState) {
-    if (buttonState == HIGH) {
-      Serial.println("on");
-      //liberar servo pra rodar rexona dps d 500 ms 
-      //+ 500ms liberar faisca com o relê
-    } else {
-      Serial.println("off");
-      //apaga td por safety
+    switch (codigo) {
+      //enter
+      case 551494365:
+        servo.write(170);
+        delay(500);
+        servo.write(0);
+        break;
     }
-    delay(50);
+
+    /*
+      buttonState = digitalRead(buttonPin);
+
+      if (buttonState != lastButtonState) {
+      if (buttonState == HIGH) {
+        Serial.println("on");
+        //liberar servo pra rodar rexona dps d 500 ms
+        //+ 500ms liberar faisca com o relê
+      } else {
+        Serial.println("off");
+        //apaga td por safety
+      }
+      delay(50);
+      }
+    */
   }
 }
